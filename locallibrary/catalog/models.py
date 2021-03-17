ab131d2d2b87f5a943b6acd8335b2a7bc0d30af4
Pavel <3410914@gmail.com>
2021-03-17 17:46:31 +0300

    part5

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -10,14 +10,22 @@ class Genre(models.Model):
         return self.name
 
 
-class Book(models.Model):
+class Language(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
 
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Enter a brief description in the file.')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -25,10 +33,14 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detaild', args=[str(self.id)])
 
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text = 'Unique ID for this particular book across whole library')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -50,7 +62,6 @@ class BookInstance(models.Model):
 
 
 class Author(models.Model):
-
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
@@ -61,9 +72,3 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
-
-
-
-
-
-
